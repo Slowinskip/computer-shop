a459394bc15e83f95cb7a47e17155754084ea2bb
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-03-05 18:01:54 +0100

    Add html and css to Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import Footer from './components/layout/Footer/Footer'
 import Home from './components/layout/Home/Home'
-import Navbar from './components/layout/Navbar/Navbar'
+import NavBar from './components/layout/Navbar/Navbar'
 
 function App() {
   return (
     <main>
-      <Navbar />
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/layout/Navbar/Navbar.js
+++ src/components/layout/Navbar/Navbar.js
@@ -1,7 +1,59 @@
 import React from 'react'
-
-const Navbar = () => {
-  return <div>Navbar</div>
+import { Button, Col, Container, Form, Nav, Row } from 'react-bootstrap'
+import styles from './Navbar.module.scss'
+import Navbar from 'react-bootstrap/Navbar'
+import { Link } from 'react-router-dom'
+import { BsCart, BsSearch } from 'react-icons/bs'
+const NavBar = () => {
+  return (
+    <Navbar className={styles.navbar}>
+      <Container>
+        <Navbar.Brand href="/">
+          <div class={styles.logo_div}>
+            <button href="/">Comp-Shop</button>
+          </div>
+        </Navbar.Brand>
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className={'justify-content-end ' + styles.colapse}
+        >
+          <Col className="col-3">
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                className={'me-2 '}
+              />
+              <Button className={styles.bnt} variant="outline-secondary">
+                <BsSearch />
+              </Button>
+            </Form>
+          </Col>
+          <Col className="col-1">
+            <Link
+              to="/cart"
+              className={'d-flex align-items-center justify-content-around '}
+            >
+              <Button className={styles.bnt} variant="outline-secondary">
+                <BsCart />
+              </Button>
+            </Link>
+          </Col>
+          <Col className={'col-2 '}>
+            <p
+              className={
+                'd-flex align-items-center justify-content-center m-0 ' +
+                styles.cartLenght
+              }
+            >
+              0
+            </p>
+          </Col>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
 
-export default Navbar
+export default NavBar
